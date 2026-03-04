de6425b91ef8dcf1bcc43c8a42b3439906d5aed0
Asphodeluv <seilahaa@gmail.com>
2026-03-04 14:34:56 +0200

    feat(AppTest.java): Add Java util imports

--- java-project/test/AppTest.java
+++ java-project/test/AppTest.java
@@ -8,3 +8,7 @@ import java.util.Arrays;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Function;
